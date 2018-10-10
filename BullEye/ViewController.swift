1fe1ec62fa5194b37ef40d10fb567178ca3a500a
Anastasia <anastasia@MacBook-Pro.local>
2018-10-10 14:43:37 +0300

    labels and variables of the scale

--- BullEye/ViewController.swift
+++ BullEye/ViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+    @IBAction func showAlert(){
+       
+        let messege = "yor value is: \(currentValue)"
+        let alert = UIAlertController(title: "hi", message: messege, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    @IBAction func SliderMoved( slider: UISlider){
+        print ("yor value is: \(slider.value)")
+        let RoundedValue = slider.value.rounded()
+        print ("The rounded your value is: \(RoundedValue)")
+        currentValue = Int (RoundedValue)
+    }
+    
 
 
 }
