d02fea33f01e05024d6a69db1ba3b004556c5c38
Anastasia <anastasia@Bogdans-iMac.local>
2018-10-12 11:46:01 +0300

    new view controller

---
+++ BullEye/AboutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.swift
+//  BullEye
+//
+//  Created by Anastasia on 10/12/18.
+//  Copyright © 2018 Anastasia. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close () {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
