c653eccd701d30333d89b1fa0815d70ffda49c1d
Anastasia <anastasia@Bogdans-iMac.local>
2018-10-12 11:57:59 +0300

    third view controler done by me

---
+++ BullEye/AboutMeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutMeViewController.swift
+//  BullEye
+//
+//  Created by Anastasia on 10/12/18.
+//  Copyright © 2018 Anastasia. All rights reserved.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func aboutme () {
+        
+    }
+
+}
